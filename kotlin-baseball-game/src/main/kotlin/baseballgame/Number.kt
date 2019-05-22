235a003ac935c092564145ce422134cdd9d4c262
hongsii <hongsii.dev@gmail.com>
2019-05-23 01:23:18 +0900

    kotlin-baseball-game : 숫자 모델 추가
    - 생성 시 범위 체크
    - 캐싱

---
+++ kotlin-baseball-game/src/main/kotlin/baseballgame/Number.kt
@@ -0,0 +1,19 @@
+package baseballgame
+
+class Number private constructor(val value: Int) {
+
+    init {
+        require(value in MIN..MAX) { "$MIN~$MAX 까지의 숫자만 가능합니다."}
+    }
+
+    companion object {
+
+        const val MIN = 0
+        const val MAX = 9
+
+        private val CACHE: MutableMap<Int, Number> = mutableMapOf()
+
+        @JvmStatic
+        fun of(value: Int): Number = CACHE.getOrPut(value) { Number(value) }
+    }
+}
---
+++ kotlin-baseball-game/src/test/kotlin/baseballgame/NumberTest.groovy
@@ -0,0 +1,38 @@
+package baseballgame
+
+import spock.lang.Specification
+
+class NumberTest extends Specification {
+
+    def "Number must be in valid range"() {
+        when:
+        Number.of(number)
+
+        then:
+        notThrown()
+
+        where:
+        number     | _
+        Number.MIN | _
+        Number.MAX | _
+    }
+
+    def "If number is in invalid range, fail to create"() {
+        when:
+        Number.of(number)
+
+        then:
+        thrown(IllegalArgumentException)
+
+        where:
+        number         | _
+        Number.MIN - 1 | _
+        Number.MAX + 1 | _
+    }
+
+    def "Number can be cached"() {
+        expect:
+        Number.of(Number.MIN) == Number.of(Number.MIN)
+        new Number(Number.MIN) != new Number(Number.MIN)
+    }
+}
