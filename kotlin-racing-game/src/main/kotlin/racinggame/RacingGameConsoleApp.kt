5e0d559870a6ddb1ec8f66a5950545ee496175ed
hongsii <hongsii.dev@gmail.com>
2019-06-15 17:12:42 +0900

    kotlin-racing-game : [refactor] 메소드 추출
    - RacingGameConsoleApp의 중복 제거

--- kotlin-racing-game/src/main/kotlin/racinggame/RacingGameConsoleApp.kt
+++ kotlin-racing-game/src/main/kotlin/racinggame/RacingGameConsoleApp.kt
@@ -3,17 +3,36 @@ package racinggame
 import racinggame.model.RacingGame
 
 fun main() {
-    println("자동차 대수는 몇 대 인가요?")
-    val countOfCar = readLine()!!.toInt()
+    val countOfCar = inputNumberWithMessage("자동차 대수는 몇 대 인가요?")
     val racingGame = RacingGame(countOfCar)
 
-    println("시도할 회수는 몇 회 인가요?")
-    val trialCount = readLine()!!.toInt()
+    val trialCount = inputNumberWithMessage("시도할 회수는 몇 회 인가요?")
     repeat(trialCount) {
         val positions = racingGame.moveOneTime()
-        println("=".repeat(trialCount))
-        positions.forEach { println("-".repeat(it)) }
-        println("=".repeat(trialCount))
+
+        println()
+        displayTrack(trialCount)
+        displayPositionOfCars(positions)
+        displayTrack(trialCount)
         println()
     }
+}
+
+private fun inputNumberWithMessage(displayMessage: String): Int {
+    while (true) {
+        try {
+            println(displayMessage)
+            return readLine()!!.toInt()
+        } catch (e: Exception) {
+            println("잘못 입력하셨습니다.")
+        }
+    }
+}
+
+private fun displayPositionOfCars(positions: List<Int>) {
+    positions.forEach { println("-".repeat(it)) }
+}
+
+private fun displayTrack(trialCount: Int) {
+    println("=".repeat(trialCount))
 }
