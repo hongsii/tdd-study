9240b1d856ab824d178c1da1a2c9acf6522d3991
hongsii <hongsii.dev@gmail.com>
2019-06-16 21:34:39 +0900

    kotlin-racing-game : [refactor] CarTest 테스트명 변경

--- kotlin-racing-game/src/test/kotlin/racinggame/model/CarTest.kt
+++ kotlin-racing-game/src/test/kotlin/racinggame/model/CarTest.kt
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments.arguments
 import org.junit.jupiter.params.provider.MethodSource
+import racinggame.model.Car.Companion.MAX_CONDITION
+import racinggame.model.Car.Companion.MIN_CONDITION
+import racinggame.model.Car.Companion.MOVE_CONDITION
 
 class CarTest {
 
@@ -17,8 +20,8 @@ class CarTest {
         car = Car()
     }
 
-    @DisplayName("If value is more than move condition, car move")
-    @ParameterizedTest(name = "[{index}] : value = {0} -> position = {1}")
+    @DisplayName("If value is more than move condition($MOVE_CONDITION), car move")
+    @ParameterizedTest(name = "[{index}] : value = {0} -> position +{1}")
     @MethodSource
     fun move(value: Int, expected: Int) {
         val position = car.move(value)
@@ -26,7 +29,7 @@ class CarTest {
         assertThat(position).isEqualTo(expected)
     }
 
-    @DisplayName("Value must be in valid range")
+    @DisplayName("If value is out of range($MIN_CONDITION~$MAX_CONDITION), throw exception")
     @ParameterizedTest(name = "[{index}] : value = {0}")
     @MethodSource
     fun moveByInvalidValue(value: Int) {
@@ -37,15 +40,15 @@ class CarTest {
 
         @JvmStatic
         fun move()= listOf(
-            arguments(Car.MOVE_CONDITION + 1, 1),
-            arguments(Car.MOVE_CONDITION, 1),
-            arguments(Car.MOVE_CONDITION - 1, 0)
+            arguments(MOVE_CONDITION + 1, 1),
+            arguments(MOVE_CONDITION, 1),
+            arguments(MOVE_CONDITION - 1, 0)
         )
 
         @JvmStatic
         fun moveByInvalidValue()= listOf(
-            arguments(Car.MAX_CONDITION + 1),
-            arguments(Car.MIN_CONDITION - 1)
+            arguments(MAX_CONDITION + 1),
+            arguments(MIN_CONDITION - 1)
         )
     }
 }
