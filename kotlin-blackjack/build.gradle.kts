acd446b6d39650ac2d45ac9e1927dc3773e37228
hongsii <hongsii.dev@gmail.com>
2019-11-05 01:00:33 +0900

    kotlin-blackjack : 프로젝트 생성

---
+++ kotlin-blackjack/build.gradle.kts
@@ -0,0 +1,22 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.41"
+}
+
+group = "dev.hongsii"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
+    testImplementation("org.assertj:assertj-core:3.11.1")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
